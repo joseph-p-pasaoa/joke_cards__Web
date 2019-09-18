9378a9d18fc2ba2104569be2118a3ee54bbb7ce1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-09-18 19:36:24 -0400

    v1.8, completed w/ bonuses

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
 
 
 
+let usedJokesObj = {};
+
 document.addEventListener("DOMContentLoaded", () => {
-  drawTen();
+  pageDraw(document.querySelector('#category-select').value);
 
-  let buttonDrawTen = document.querySelector("#button-draw10");
-  buttonDrawTen.addEventListener("click", drawTen);
+  let buttonDrawJokes = document.querySelector("#button-draw-jokes");
+  buttonDrawJokes.addEventListener("click", () => {
+      pageDraw(document.querySelector('#category-select').value);
+  });
 
-  let cardsFlex = document.querySelector("#cards-flex");
+  // PUNCHLINE TRIGGER SYSTEM
+  const cardsFlex = document.querySelector("#cards-flex");
   cardsFlex.addEventListener('click', (e) => {
       if (e.target.parentNode.className === "joke-card") {
         let punchline = e.target.parentNode.childNodes[1];
@@ -22,6 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+
 const clearJokes = () => {
   const cardsFlex = document.querySelector("#cards-flex");
   while (cardsFlex.firstChild) {
@@ -29,34 +36,16 @@ const clearJokes = () => {
   }
 }
 
-function drawTen() {
-  clearJokes();
-  
-  fetch("https://official-joke-api.appspot.com/random_ten")
-    .then(response => {
-        if (response.status !== 200) {
-          return alert(`Error: ${response.status}. Please try again later.`);
-        } else {
-          return response.json();
-        }
-    })
-    .then(jokesArr => {
-        jokesArr.forEach(jokeObj => {
-            makeCard(jokeObj);
-        });
-        let cardsFlexArr = document.querySelector("#cards-flex").childNodes;
-        for (let i = 0; i < cardsFlexArr.length; i++) {
-          setTimeout(() => {
-            cardsFlexArr[i].setAttribute('style', 'opacity: 1;');
-          }, i * 40);
-        }
-    })
-    .catch(err => {
-      console.log("err: ", err);
-    });
+function errorOrJson(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
 }
 
 const makeCard = (jokeObj) => {
+  usedJokesObj[jokeObj.id] = true;
+
   let cardsFlex = document.querySelector("#cards-flex");
 
   let buildingCard = document.createElement("div");
@@ -72,4 +61,48 @@ const makeCard = (jokeObj) => {
   buildingCard.appendChild(buildingCardSetup);
   buildingCard.appendChild(buildingCardPunch);
   cardsFlex.appendChild(buildingCard);
+}
+
+function drawTenJokes(category) {
+  fetch(`https://official-joke-api.appspot.com/jokes/${category}ten`)
+    .then(errorOrJson)
+    .then(jokesArr => {
+        // BUILDING CURRENT JOKES POOL 1OF2
+        fetchedJokesArr = jokesArr;
+        return fetch(`https://official-joke-api.appspot.com/jokes/${category}ten`);
+    })
+    .then(errorOrJson)
+    .then(jokesArr2 => {
+        // BUILDING CURRENT JOKES POOL 2OF2
+        fetchedJokesArr = fetchedJokesArr.concat(jokesArr2);
+
+        // CREATES JOKE CARDS IN DOM USING JOKES POOL
+        let cardsFlexArr = document.querySelector("#cards-flex").childNodes;
+        while (cardsFlexArr.length < 10) {
+          if (fetchedJokesArr.length < 1) {
+            usedJokesObj = {};
+          } 
+          if (!usedJokesObj[fetchedJokesArr[0].id]) {
+            makeCard(fetchedJokesArr[0]);
+          }
+          fetchedJokesArr.shift();
+        }
+
+        // REVEALS JOKES CARDS IN QUICK SERIES
+        for (let i = 0; i < cardsFlexArr.length; i++) {
+          setTimeout(() => {
+            cardsFlexArr[i].setAttribute('style', 'opacity: 1;');
+          }, i * 40);
+        }
+    })
+    .catch(err => {
+        console.log("Fetch error: ", err);
+    });
+}
+
+function pageDraw(category) {
+  clearJokes();
+
+  let fetchedJokesArr = [];
+  drawTenJokes(category);
 }
